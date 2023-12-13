4289d432f84b753047c58f2c22be4cd6eab5f9f5
pubrondeau <pubrondeau@gmail.com>
2023-12-13 15:18:20 +0100

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -10,8 +10,8 @@ module.exports = {
   author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
   authorEmail: '', // i.e. hola@lenesaile.com - email of the author
   authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
-  themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
-  themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
+  themeColor: '#E54848', //  Manifest: defines the default theme color for the application
+  themeBgColor: '#FFFDF5', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
     opengraph_default_alt:
